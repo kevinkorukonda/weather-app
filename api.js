e30c58ccd73b416253f7e1bc41dd907a95ab13d3
kevinkorukonda <138764944+kevinkorukonda@users.noreply.github.com>
2025-01-13 08:57:24 -0800

    Add files via upload

---
+++ api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "501b9bf27bmshdd41a244c876d22p132f5cjsnbe919945b2f7",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "18c9f74571c062105c170d4fd1fcceb3"; // enter your key from openweather API
